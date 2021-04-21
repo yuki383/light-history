038c980a62a803809551deee5116077d0d9a9219
yuu383 <yuukikokubo.pg@gmail.com>
2021-04-22 03:20:33 +0900

    prettier write

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   root: true,
-	env: {
-		browser: true,
-		node: true,
-		es2021: true,
-	},
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
